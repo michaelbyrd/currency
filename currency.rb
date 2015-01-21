b1dc66c977e5cdd7794aabbc397684d35d3e7699
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-01-20 19:54:42 -0500

    initial commit, but it is a bit late

---
+++ currency.rb
@@ -0,0 +1,45 @@
+class Currency
+  attr_reader :amount, :code
+  def initialize(amount, code)
+    @amount = amount
+    @code = code
+  end
+
+  def ==(currency)
+    self.code == currency.code && self.amount == currency.amount
+  end
+
+  def +(currency)
+    if self.code == currency.code
+      total = self.amount + currency.amount
+      code = self.code
+      Currency.new(total, code)
+    else
+      raise 'DifferentCurrencyCodeError'
+    end
+  end
+  def -(currency)
+    if self.code == currency.code
+      total = self.amount - currency.amount
+      code = self.code
+      Currency.new(total, code)
+    else
+      raise 'DifferentCurrencyCodeError'
+    end
+  end
+
+  def *(number)
+    if number.class == Fixnum
+      total = self.amount * number
+      Currency.new(total, self.code)
+    elsif number.class == Float
+      total = self.amount * number
+      Currency.new(total, self.code)
+    else
+      raise 'WrongTypeOfArgument'
+    end
+  end
+
+
+
+end
---
+++ currency_test.rb
@@ -0,0 +1,83 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './currency'
+
+class CurrencyTest < MiniTest::Test
+  def test_currency_class_exist
+    assert Currency
+  end
+
+  def test_currency_knows_its_amount_and_code
+    a = Currency.new(10, :usd)
+    assert_equal 10, a.amount
+    assert_equal :usd, a.code
+  end
+
+  def test_currency_can_be_equivalent
+    a = Currency.new(10, :usd)
+    b = Currency.new(10, :usd)
+    c = Currency.new(10, :jpy)
+    assert a == b
+    refute a == c
+  end
+
+  def test_that_currency_with_the_same_code_can_be_added
+    a = Currency.new(5, :usd)
+    b = Currency.new(5, :usd)
+    result = a + b
+    assert_equal 10, result.amount
+    refute 11 == result.amount
+    assert_equal :usd, result.code
+  end
+
+  def test_that_currency_with_different_codes_can_not_be_added
+    a = Currency.new(5, :usd)
+    b = Currency.new(5, :jpy)
+    begin
+    a + b
+    rescue Exception => ex
+      assert_equal 'DifferentCurrencyCodeError', ex.message
+    end
+  end
+
+  def test_that_currency_with_the_same_code_can_be_subtracted
+    a = Currency.new(5, :usd)
+    b = Currency.new(2, :usd)
+    result = a - b
+    assert_equal 3, result.amount
+    assert_equal :usd, result.code
+  end
+
+  def test_that_currency_with_different_codes_can_not_be_subtracted
+    a = Currency.new(5, :usd)
+    b = Currency.new(3, :jpy)
+    begin
+    a - b
+    rescue Exception => ex
+      assert_equal 'DifferentCurrencyCodeError', ex.message
+    end
+  end
+
+  def test_that_currency_can_be_multiplied_by_a_fixnum
+    a = Currency.new(10, :usd)
+    result = a*10
+    assert_equal 100, result.amount
+  end
+
+  def test_that_currency_can_be_multiplied_by_a_float
+    a = Currency.new(10, :usd)
+    result = a*1.5
+    assert_equal 15.0, result.amount
+    assert_equal Float, result.amount.class
+  end
+
+  def test_that_current_can_not_be_multiplied_by_other_objects
+    a = Currency.new(10, :usd)
+    begin
+      a*'string'
+    rescue Exception => ex
+      assert_equal 'WrongTypeOfArgument', ex.message
+    end
+  end
+
+end
